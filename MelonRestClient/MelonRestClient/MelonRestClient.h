6cb420e42aeeadf80063bbebd2a1a091f7d9d32f
Tomek Popis <tomek@melon-it.com>
2016-02-07 18:02:41 +0100

    Configure project

---
+++ MelonRestClient/MelonRestClient/MelonRestClient.h
@@ -0,0 +1,19 @@
+//
+//  MelonRestClient.h
+//  MelonRestClient
+//
+//  Created by Tomasz Popis on 07/02/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MelonRestClient.
+FOUNDATION_EXPORT double MelonRestClientVersionNumber;
+
+//! Project version string for MelonRestClient.
+FOUNDATION_EXPORT const unsigned char MelonRestClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MelonRestClient/PublicHeader.h>
+
+
